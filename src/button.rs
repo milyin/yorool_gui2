03e55b6e75a95a8e13997145b419980bc5758851
Michael Ilyin <milyin@gmail.com>
2020-01-17 14:39:57 +0300

    checkbox

--- src/button.rs
+++ src/button.rs
@@ -4,9 +4,24 @@ use ggez::graphics::{self, Align, DrawMode, DrawParam, MeshBuilder, Rect, Text};
 use ggez::nalgebra::Point2;
 use ggez::{Context, GameResult};
 
+#[derive(Clone)]
+pub enum Mode {
+    Button,
+    Checkbox(bool),
+    Radio(bool),
+    //Checkbox3State(), - or even N-state, may be added later
+}
+
+impl Default for Mode {
+    fn default() -> Self {
+        Mode::Checkbox(false)
+    }
+}
+
 #[derive(Clone, Default)]
 pub struct State {
-    pressed: bool,
+    mode: Mode,
+    touched: bool,
     label: String,
     rect: Rect,
 }
@@ -24,8 +39,16 @@ pub struct DefaultSkin {
 const MARGIN: f32 = 5.;
 const PRESS_OFFSET: f32 = 10.;
 
-fn button_rect(mut rect: Rect, pressed: bool) -> Rect {
-    let dxy = if pressed { PRESS_OFFSET } else { 0. };
+fn base_rect(mut rect: Rect) -> Rect {
+    rect.x += MARGIN;
+    rect.y += MARGIN;
+    rect.w -= MARGIN * 2.;
+    rect.h -= MARGIN * 2.;
+    rect
+}
+
+fn button_rect(mut rect: Rect, touched: bool) -> Rect {
+    let dxy = if touched { PRESS_OFFSET } else { 0. };
     rect.x += MARGIN + dxy;
     rect.y += MARGIN + dxy;
     rect.w -= MARGIN * 2. + PRESS_OFFSET;
@@ -39,19 +62,40 @@ impl EventHandler for DefaultSkin {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let rect = button_rect(self.state.rect, self.state.pressed);
-        let mesh = MeshBuilder::new()
-            .rectangle(DrawMode::fill(), rect, graphics::WHITE)
-            .build(ctx)?;
-        graphics::draw(ctx, &mesh, DrawParam::default())?;
-        let mut text = Text::new(self.state.label.clone());
-        text.set_bounds([rect.w, rect.h], Align::Center);
-        let tdh = (rect.h - text.height(ctx) as f32) / 2.;
-        graphics::draw(
-            ctx,
-            &text,
-            (Point2::new(rect.x, rect.y + tdh), graphics::BLACK),
-        )
+        match &self.state.mode {
+            Mode::Button => {
+                let rect = button_rect(self.state.rect, self.state.touched);
+                let mesh = MeshBuilder::new()
+                    .rectangle(DrawMode::fill(), rect, graphics::WHITE)
+                    .build(ctx)?;
+                graphics::draw(ctx, &mesh, DrawParam::default())?;
+                let mut text = Text::new(self.state.label.clone());
+                text.set_bounds([rect.w, rect.h], Align::Center);
+                let tdh = (rect.h - text.height(ctx) as f32) / 2.;
+                graphics::draw(
+                    ctx,
+                    &text,
+                    (Point2::new(rect.x, rect.y + tdh), graphics::BLACK),
+                )
+            }
+            Mode::Checkbox(checked) => {
+                let rect = base_rect(self.state.rect);
+                let mesh = MeshBuilder::new()
+                    .rectangle(
+                        if *checked {
+                            DrawMode::fill()
+                        } else {
+                            DrawMode::stroke(1.)
+                        },
+                        rect,
+                        graphics::WHITE,
+                    )
+                    .build(ctx)?;
+                graphics::draw(ctx, &mesh, DrawParam::default())
+            }
+
+            _ => panic!(),
+        }
     }
 }
 
@@ -60,7 +104,7 @@ impl Skin for DefaultSkin {
         self.state = state.clone();
     }
     fn is_hot_area(&self, x: f32, y: f32) -> bool {
-        let rect = button_rect(self.state.rect, self.state.pressed);
+        let rect = base_rect(self.state.rect);
         rect.contains(Point2::new(x, y))
     }
 }
@@ -100,15 +144,19 @@ impl<S: Skin> EventHandler for Button<S> {
 
     fn mouse_button_down_event(&mut self, ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
         if self.skin.is_hot_area(x, y) {
-            self.state.pressed = true;
+            self.state.touched = true;
         }
     }
 
     fn mouse_button_up_event(&mut self, ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
-        if self.state.pressed {
-            self.state.pressed = false;
+        if self.state.touched {
+            self.state.touched = false;
             if self.skin.is_hot_area(x, y) {
-                // fire onclick event
+                match &self.state.mode {
+                    Mode::Button => {}
+                    Mode::Checkbox(check) => self.state.mode = Mode::Checkbox(!*check),
+                    _ => panic!(),
+                }
             }
         }
     }
